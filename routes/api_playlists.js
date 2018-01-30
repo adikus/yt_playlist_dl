07411936b7aae664135a33812153b5bf0d652abb
adikus <andrej.hoos@gmail.com>
2018-01-30 11:42:38 +0100

    Fix /api_playlists

--- routes/api_playlists.js
+++ routes/api_playlists.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const _ = require('lodash');
+const _fp = require('lodash/fp');
 
 const Playlist = require('./../models/playlist');
+const Video = require('./../models/video');
 const wrap = require('./../lib/express-router-promise').wrap;
 
 const router = express.Router();
 
 router.get('/', wrap(async function(req, res) {
-    let playlists = await req.models.playlist.find({user_id: req.user.id}, {order: '-created_at'});
-    res.json({playlists: playlists});
+    let playlists = await req.models.playlist.findAsync({user_id: req.user.id}, {order: '-created_at'});
+    let playlistJson = _fp.map(_fp.pick(_.keys(req.models.playlist.properties)), playlists);
+    res.json({playlists: playlistJson});
 }));
 
 router.get('/:id', wrap(async (req, res) => {
@@ -17,8 +20,20 @@ router.get('/:id', wrap(async (req, res) => {
         return res.send(401);
     }
     let videos = await playlist.getVideos();
+    await Video.preload(req, videos, 'original_upload_id', 'originalUpload', 'upload');
+    await Video.preload(req, videos, 'mp3_upload_id', 'mp3Upload', 'upload');
 
-    res.json({playlist: playlist, items: videos});
+    let playlistJson = _(playlist).pick(_.keys(req.models.playlist.properties));
+    _(videos).each((video) => {
+        video.playlistVideo = _(video.playlistVideo).pick(_.keys(req.models.playlist_video.properties));
+        video.originalUpload = _(video.originalUpload).pick(_.keys(req.models.upload.properties));
+        video.mp3Upload = _(video.mp3Upload).pick(_.keys(req.models.upload.properties));
+    });
+    let videoColumns = _.keys(req.models.video.properties);
+    videoColumns.push('originalUpload', 'mp3Upload', 'playlistVideo');
+    let videosJson = _fp.map(_fp.pick(videoColumns), videos);
+
+    res.json({playlist: playlistJson, items:videosJson});
 }));
 
 
