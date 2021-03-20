6805406e9d74e3b946b8b92ee02f81a5a4823973
Andrej Hoos <andrej.hoos@gmail.com>
2021-03-20 19:33:29 +0100

    Add download-all and convert-all cron endpoints

--- routes/cron.js
+++ routes/cron.js
@@ -4,8 +4,10 @@ const util = require('util');
 
 const ytOauth = require('./../services/yt_oauth');
 const wrap = require('./../lib/express-router-promise').wrap;
+const asyncPromise = require('./../lib/async-promise');
 
 const Playlist = require('./../models/playlist');
+const Video = require('./../models/video');
 
 const router = express.Router();
 
@@ -23,6 +25,57 @@ router.get('/refresh-all', wrap(async function(req, res) {
         for(let playlist of playlists) {
             console.log('Refreshing', playlist.title);
             await playlist.refresh(req);
+        }
+    }
+
+    res.json({status: 'ok'});
+}));
+
+router.get('/download-all', wrap(async function(req, res) {
+    let users = await req.models.user.allAsync();
+
+    for(let user of users) {
+        console.log('Downloading playlists for', user.username);
+        req.user = user;
+        let playlists = await req.models.playlist.findAsync({user_id: req.user.id, autoupdate: true});
+        for(let playlist of playlists) {
+            console.log('Downloading', playlist.title);
+            let videos = await playlist.getVideos();
+            await Video.preload(req, videos, 'original_upload_id', 'originalUpload', 'upload');
+            await asyncPromise.eachLimit(videos, 50, async function iteratee(video) {
+                if(!video.originalUpload) {
+                    await video.uploadToS3();
+                }
+            });
+        }
+    }
+
+    res.json({status: 'ok'});
+}));
+
+router.get('/convert-all', wrap(async function(req, res) {
+    let users = await req.models.user.allAsync();
+
+    for(let user of users) {
+        console.log('Converting playlists for', user.username);
+        req.user = user;
+        let playlists = await req.models.playlist.findAsync({user_id: req.user.id, autoupdate: true});
+        for(let playlist of playlists) {
+            console.log('Converting', playlist.title);
+            let videos = await playlist.getVideos();
+            await Video.preload(req, videos, 'mp3_upload_id', 'mp3Upload', 'upload');
+
+            await asyncPromise.eachLimit(videos, 200, async function iteratee(video) {
+                if(!video.mp3Upload){
+                    try {
+                        await video.convertAndUploadToS3();
+                    } catch (err) {
+                        console.error(err);
+                    }
+                }
+            });
+
+            console.log('Done');
         }
     }
 
