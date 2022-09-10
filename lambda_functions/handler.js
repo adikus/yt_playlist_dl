7492a9474557c2460c9fd69491f422dc82dc5d7a
Andrej Hoos <andrej.hoos@gmail.com>
2022-09-10 20:53:07 +0200

    Rewrite lambda to be docker based :white_check_mark: + debug log

--- lambda_functions/handler.js
+++ lambda_functions/handler.js
@@ -95,6 +95,7 @@ async function downloadFromS3(key, filename) {
 }
 
 module.exports.convert = async function(event, context, callback) {
+    console.log(event);
     let params = JSON.parse(event.body || "{}");
 
     console.log("Invoking convert function with params", params);
