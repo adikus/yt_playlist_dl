32ad92580d0286906afd8c3223ce3377c1d03a1b
Andrej Hoos <andrej.hoos@gmail.com>
2022-09-10 21:11:32 +0200

    Rewrite lambda to be docker based - fix event params :sweep:

--- lambda_functions/convert.js
+++ lambda_functions/convert.js
@@ -1,5 +1,5 @@
 const handlers = require('./handler');
 
-handlers.convert({ body: JSON.stringify({ source_key: process.argv[2], dest_key: process.argv[3] }) }, null, (result) => {
+handlers.convert({ source_key: process.argv[2], dest_key: process.argv[3] }, null, (result) => {
     console.log(result);
 });
--- lambda_functions/resolve.js
+++ lambda_functions/resolve.js
@@ -1,5 +1,5 @@
 const handlers = require('./handler');
 
-handlers.resolve({ body: JSON.stringify({ id: process.argv[2], key: process.argv[3] }) }, null, (result) => {
+handlers.resolve({ id: process.argv[2], key: process.argv[3] }, null, (result) => {
     console.log(result);
 });
