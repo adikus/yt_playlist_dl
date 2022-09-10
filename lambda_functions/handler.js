d28dd0934b89d2652ed396851dc1b161eac1be3b
Andrej Hoos <andrej.hoos@gmail.com>
2022-09-10 21:11:50 +0200

    Rewrite lambda to be docker based - fix ffmpeg command

--- lambda_functions/handler.js
+++ lambda_functions/handler.js
@@ -104,7 +104,7 @@ module.exports.convert = async function(event, context, callback) {
     console.log('Downloaded, converting...');
 
     let dest_filename = `/tmp/${crypto.randomBytes(16).toString('hex')}`;
-    await execPromise(`/var/task/bin/ffmpeg/ffmpeg -y -i ${download_filename} -f mp3 -b:a 128k ${dest_filename}`);
+    await execPromise(`ffmpeg -y -i ${download_filename} -f mp3 -b:a 128k ${dest_filename}`);
 
     let file = await util.promisify(fs.readFile)(dest_filename);
 
