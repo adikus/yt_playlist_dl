3932571c6d046dc722d42f71bf56023f694f5e05
Andrej Hoos <andrej.hoos@gmail.com>
2022-10-15 15:59:18 +0200

    Refactor duplication in playlists router

--- server/routers/playlists.js
+++ server/routers/playlists.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 
 const Playlist = require('./../models/playlist');
 const Video = require('./../models/video');
-const YtPlaylist = require('./../models/yt_playlist');
 const exporter = require('./../services/exporter');
 const wrap = require('./../lib/express-router-promise').wrap;
 const asyncPromise = require('./../lib/async-promise');
@@ -20,13 +19,17 @@ router.get('/refresh', wrap(async function(req, res) {
     res.redirect('/playlists');
 }));
 
-router.get('/:id', wrap(async function(req, res) {
-    // TODO: extract common stuff
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
+router.get('/:id*', wrap(async function(req, res, next) {
+    req.playlist = await Playlist.getFromDbOrApi(req, req.params.id);
+    if(req.playlist.user_id !== req.user.id) {
         return res.send(401);
     }
-    let videos = await playlist.getVideos();
+
+    next()
+}));
+
+router.get('/:id', wrap(async function(req, res) {
+    let videos = await req.playlist.getVideos();
 
     await Video.preload(req, videos, 'original_upload_id', 'originalUpload', 'upload');
     await Video.preload(req, videos, 'mp3_upload_id', 'mp3Upload', 'upload');
@@ -35,29 +38,20 @@ router.get('/:id', wrap(async function(req, res) {
     if (videos.length === 0){
         res.redirect('/playlists/' + req.params.id + '/refresh');
     } else {
-        res.render('playlist', {title: playlist.title, playlist: playlist, items: videos});
+        res.render('playlist', {title: req.playlist.title, playlist: req.playlist, items: videos});
     }
 }));
 
 router.get('/:id/refresh', wrap(async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
-    await playlist.refresh(req);
+    await req.playlist.refresh(req);
 
-    res.redirect('/playlists/' + playlist.id);
+    res.redirect('/playlists/' + req.playlist.id);
 }));
 
 router.get('/:id/upload', async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
+    res.redirect('/playlists/' + req.playlist.id);
 
-    res.redirect('/playlists/' + playlist.id);
-
-    let videos = await playlist.getVideos();
+    let videos = await req.playlist.getVideos();
     await asyncPromise.eachLimit(videos, 50, async function iteratee(video) {
         let upload = await video.getUpload();
         if(!upload) {
@@ -71,14 +65,9 @@ router.get('/:id/upload', async function(req, res) {
 });
 
 router.get('/:id/convert', async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
+    res.redirect('/playlists/' + req.playlist.id);
 
-    res.redirect('/playlists/' + playlist.id);
-
-    let videos = await playlist.getVideos();
+    let videos = await req.playlist.getVideos();
     await Video.preload(req, videos, 'mp3_upload_id', 'mp3Upload', 'upload');
 
     await asyncPromise.eachLimit(videos, 200, async function iteratee(video) {
@@ -97,28 +86,18 @@ router.get('/:id/convert', async function(req, res) {
 });
 
 router.get('/:id/export', wrap(async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
-
-    let videos = await playlist.getVideos();
+    let videos = await req.playlist.getVideos();
     await Video.preload(req, videos, 'mp3_upload_id', 'mp3Upload', 'upload');
     videos = _(videos).filter((video) => video.mp3Upload && video.mp3Upload.file).value();
-    res.render('export', {title: 'Export ' + playlist.title, playlist: playlist, items: videos});
+    res.render('export', {title: 'Export ' + req.playlist.title, playlist: req.playlist, items: videos});
 }));
 
 router.post('/:id/export', wrap(async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
-
-    let videos = await playlist.getVideos();
-    let stream = await exporter.exportAlbum(playlist, videos, req.body.index);
+    let videos = await req.playlist.getVideos();
+    let stream = await exporter.exportAlbum(req.playlist, videos, req.body.index);
     res.writeHead(200, {
         'Content-Type': 'application/zip',
-        'Content-disposition': 'attachment;filename="' + playlist.album_name + '.zip"'
+        'Content-disposition': 'attachment;filename="' + req.playlist.album_name + '.zip"'
     });
     stream.pipe(res).on('finish', function () {
         stream.cleanUp();
@@ -128,19 +107,14 @@ router.post('/:id/export', wrap(async function(req, res) {
 }));
 
 router.post('/:id/metadata', wrap(async function(req, res) {
-    let playlist = await Playlist.getFromDbOrApi(req, req.params.id);
-    if(playlist.user_id !== req.user.id) {
-        return res.send(401);
-    }
-
-    playlist.autoupdate = req.body.autoupdate === 'on';
-    playlist.album_name = req.body.album;
-    await playlist.saveAsync();
+    req.playlist.autoupdate = req.body.autoupdate === 'on';
+    req.playlist.album_name = req.body.album;
+    await req.playlist.saveAsync();
 
     if(req.files && req.files.image_file && req.files.image_file.data.length){
-        await playlist.uploadAlbumCover(req.files.image_file);
+        await req.playlist.uploadAlbumCover(req.files.image_file);
     }
-    res.redirect('/playlists/' + playlist.id);
+    res.redirect('/playlists/' + req.playlist.id);
 }));
 
 module.exports = router;
