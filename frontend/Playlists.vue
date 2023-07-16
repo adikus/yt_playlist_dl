e9844530b7dc93be2b6a2c3957e75d0d4b9e1120
Andrej Hoos <andrej.hoos@gmail.com>
2023-07-16 14:50:54 +0200

    Re-arrange design to work on mobile :lipstick:

--- frontend/Playlists.vue
+++ frontend/Playlists.vue
@@ -1,29 +1,26 @@
 <template lang="pug">
-.row
-    .col.d-flex.flex-row.justify-content-between
-        input.form-control.flex-grow-1.w-auto.mr-2(type="text", placeholder="Search")
-        a.btn.btn-primary(href='#', @click.prevent="refresh")
-            spinner.mr-1(size="16", v-if="refreshing")
-            span.fa.fa-refresh.mr-1(v-else)
-            | Refresh playlists
-.row
-    .col
-        .border-bottom.py-2(v-for="playlist in playlists")
-            .d-flex.flex-wrap
-                img.thumbnail.mr-2(v-lazy="playlistImage(playlist)", role="button", @click.prevent="selectPlaylist(playlist)")
-                div
-                    h2(role="button", @click.prevent="selectPlaylist(playlist)")
-                        u(v-if="isSelected(playlist)") {{playlist.title}}
-                        template(v-else) {{playlist.title}}
-                    .text-muted
-                        | {{playlist.status}} | {{playlist.created_at}}
-                        spinner(size="18", fill="#212529", v-if="playlist.loading")
+.d-flex.flex-row.justify-content-between
+    input.form-control.flex-grow-1.w-auto.mr-2(type="text", placeholder="Search")
+    a.btn.btn-primary.d-none.d-md-block(href='#', @click.prevent="refresh")
+        spinner.mr-1(size="16", v-if="refreshing")
+        span.fa.fa-refresh.mr-1(v-else)
+        | Refresh playlists
+.border-bottom.py-2(v-for="playlist in playlists")
+    .d-flex.flex-wrap
+        img.thumbnail.mr-2(v-lazy="playlistImage(playlist)", role="button", @click.prevent="selectPlaylist(playlist)")
+        div
+            h2(role="button", @click.prevent="selectPlaylist(playlist)")
+                u(v-if="isSelected(playlist)") {{playlist.title}}
+                template(v-else) {{playlist.title}}
+            .text-muted
+                | {{playlist.status}} | {{playlist.created_at}}
+                spinner(size="18", fill="#212529", v-if="playlist.loading")
 
-            div.mt-2
-                .d-flex.flex-row.flex-wrap
-                    div.d-flex.flex-row.border.rounded.p-1.m-1(v-for="video in playlist.shownVideos", role="button", @click.prevent="selectVideo(video)", :class='{ "bg-grey-md": selectedVideo === video }')
-                        img.thumbnail-small.mr-2(v-lazy="videoImage(video)")
-                        small.video-title(:title="video.title") {{video.title}}
+    div.mt-2
+        .d-flex.flex-row.flex-wrap
+            div.d-flex.flex-row.border.rounded.p-1.m-1(v-for="video in playlist.shownVideos", role="button", @click.prevent="selectVideo(video)", :class='{ "bg-grey-md": selectedVideo === video }')
+                img.thumbnail-small.mr-2(v-lazy="videoImage(video)")
+                small.video-title(:title="video.title") {{video.title}}
 
 
 </template>
@@ -93,7 +90,7 @@ img.thumbnail-small {
     height: 36px;
 }
 .video-title {
-    width: 180px;
+    width: 175px;
     height: 42px;
     overflow: hidden;
 }
