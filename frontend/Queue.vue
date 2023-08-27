90b89b842cc83222428eede2099b7a3867e18aa3
Andrej Hoos <andrej.hoos@gmail.com>
2023-08-27 16:22:22 +0200

    Improved playing indicator in queue

--- frontend/Queue.vue
+++ frontend/Queue.vue
@@ -16,10 +16,11 @@
         @click.prevent="$emit('playVideo', video)"
         :class='{ "border-top": index === 0, "rounded-top": index === 0, "rounded-bottom": index === queue.length - 1 }'
     >
-        <template v-if="video.id === playingVideo.id">
-            <i class="d-inline-block w-15px fa fa-play mx-1" v-if="!playing"></i>
-            <i class="d-inline-block w-15px fa fa-pause mx-1" v-if="playing"></i>
-        </template>
+        <div v-if="video.id === playingVideo.id" class="bars mx-1" :class='{ "bars-active": playing }'>
+            <span/>
+            <span/>
+            <span/>
+        </div>
         <span v-else class="d-inline-block w-15px mx-1">&nbsp;</span>
         {{video.title}}
     </div>
@@ -50,5 +51,58 @@ a[disabled="true"] {
     color: grey !important;
     text-decoration: none;
     cursor: default;
+}
+.bars {
+    position: relative;
+    left: 1px;
+    display: inline-flex;
+    justify-content: space-between;
+    width: 15px;
+    height: 15px;
+
+    span {
+        width: 3px;
+        height: 100%;
+        background-color: black;
+        border-radius: 3px;
+        content: '';
+        transform-origin: bottom;
+        transform: scaleY(0.2);
+
+        &:nth-of-type(2) {
+            animation-delay: -2.2s; /* Start at the end of animation */
+        }
+
+        &:nth-of-type(3) {
+            animation-delay: -3.7s; /* Start mid-way of return of animation */
+        }
+    }
+}
+.bars-active {
+    @keyframes bounce {
+        10% {
+            transform: scaleY(0.3); /* start by scaling to 30% */
+        }
+
+        30% {
+            transform: scaleY(1); /* scale up to 100% */
+        }
+
+        60% {
+            transform: scaleY(0.5); /* scale down to 50% */
+        }
+
+        80% {
+            transform: scaleY(0.75); /* scale up to 75% */
+        }
+
+        100% {
+            transform: scaleY(0.6); /* scale down to 60% */
+        }
+    }
+
+    span {
+        animation: bounce 1.2s ease infinite alternate;
+    }
 }
 </style>
